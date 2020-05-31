ca788e9173500021a05f9b544217daee748dd071
samohtsns <sensethomas1@gmail.com>
2020-05-31 22:23:41 +0200

    blank

--- website/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // reprends la session
-session_destroy(); // détruit la session
-header('location: index.php'); // redirection sur la page d'accueil
-?>
-
-<!-- Si connecté alors affiché bouton déconnexion sinon afficher bouton connexion -->
