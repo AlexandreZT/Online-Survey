c25c1bc4cb8ef149a18f8cda0e01f9e5e5ec5630
samohtsns <sensethomas1@gmail.com>
2020-05-31 17:02:59 +0200

    design nom formulaire

--- website/poll-creation.php
+++ website/poll-creation.php
@@ -4,29 +4,97 @@
 ?>
 
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>formulaire</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
-  </head>
-  <body>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Création du formulaire</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  
+  <!-- Google Fonts --> 
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/survey-style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vesperr - v2.0.0
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+	<!-- ======= Header ======= -->
+	<header id="header" class="fixed-top d-flex align-items-center">
+	    <div class="container d-flex align-items-center">
+
+	      <div class="logo mr-auto">
+	        <!--<h1 class="text-light"><a href="index.html"><span>Vesperr</span></a></h1>-->
+	        <!-- Uncomment below if you prefer to use an image logo -->
+	        <a href="index.php"><img src="assets/img/banner.png" alt="" class="img-fluid"></a>
+	      </div>
+
+	      <nav class="nav-menu d-none d-lg-block">
+	        <ul>
+	          <!--<li class="active"><a href="#header">Accueil</a></li>-->
+	          <li><a href="#about">Plus d'Infos</a></li>
+	          <li><a href="#pricing">Tarification & Abonnements</a></li>
+	          <li><a href="poll-creation.php">Questionnaire</a></li>
+	
+	          <?php
+	              if(isset($_SESSION['pseudo'])) {
+	            ?> 
+	            <li class="get-started"><a href="logout.php">Déconnexion</a></li>
+	            <!-- <li class="get-started"><a href="login.php">Connexion</a></li> -->
+	            <?php
+	              }
+	              else {
+	            ?>
+	            <li class="get-started"><a href="login.php">Connexion</a></li>
+	            <!-- <li><a href="logout.php">Déconnexion</a></li> -->
+	            <?php
+	          }
+	          ?>
+	        </ul>
+	      </nav>
+	      <!-- .nav-menu -->
+
+	    </div>
+  </header>
+  <!-- End Header -->
   
   <?php
 	if (isset($_SESSION['pseudo'])) { // si un utilisateur est connecté
 		// echo "<p id='welcome'>Bonjour $_SESSION[pseudo]</p>"; 
 		// echo "<p id='welcome'>Ton id : $_SESSION[id]</p>";
 	?> 
-	
-	<form id="create_surveys" action="poll-creation.php" method="post"> <!-- Type de réponse, option obligatoire, choix de réponse-->
-		<label><b>Donner un titre à votre sondage :</b></label>
-		<br>
-        <input type="text" name="create_surveys" size="50" maxlength="100" placeholder="Saisissez un titre de sondage" value="">
-		<br>
-		<input type="submit" name="Create_Surveys" value="Créer"> <!-- onclick="redirect()" -->
-
-	</form>
+	<div id="form">
+		<form id="create_surveys" action="poll-creation.php" method="post"> <!-- Type de réponse, option obligatoire, choix de réponse-->
+			<label><b>Donner un titre à votre sondage :</b></label>
+			<br>
+    	    <input type="text" name="create_surveys" size="50" maxlength="100" placeholder="Saisissez un titre de sondage" value="">
+			<input type="submit" name="Create_Surveys" value="Créer"> <!-- onclick="redirect()" -->
+		</form>
+	</div>
 
 	<?php
 		// permet de créer un sondage
--- website/question-creation.php
+++ website/question-creation.php
@@ -4,74 +4,108 @@
 ?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-<form id="create_questions" action="question-creation.php" method="post">
-    <label>
-        <?php echo "Q";
-            if(!isset($_SESSION['qNumber'])){
-                $_SESSION['qNumber'] = 1;   
-            }
-            echo"$_SESSION[qNumber]";
-        ?>
-    </label> <!-- $qNumber-->
-    <input type="text" name="question" size="50" maxlength="100" placeholder="Saisissez votre question">
-    <!-- choix type de réponse -->
-    <select name="type" size="1" > <!-- onchange="this.form.submit()" -->
-        <!--<option value="none">type de réponse</option> no value -->
-        <option value="unique">Choix unique</option>
-        <option value="multiple">Choix multiple</option>
-        <option value="text">Zone de text</option>
-    </select>
-    <!-- option de réponse obligatoire (if empty alors invalide) -->
-    <label>obligatoire</label>
-    <input type="checkbox" name="obligatoire" value="TRUE">
-    <br>
-    <input type="submit" name="create_questions" value="Passer aux réponses">
+<html lang="fr">
 
-</form>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-<?php
-if (isset($_POST['create_questions'])) { //ok
-    if(empty($_POST['obligatoire']))
-    {
-        $obligatoire = 0; // false
+  <title>Création du formulaire</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  
+  <!-- Google Fonts --> 
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/survey-style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vesperr - v2.0.0
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+    <form id="create_questions" action="question-creation.php" method="post">
+        <label>
+            <?php echo "Q";
+                if(!isset($_SESSION['qNumber'])){
+                    $_SESSION['qNumber'] = 1;   
+                }
+                echo"$_SESSION[qNumber]";
+            ?>
+        </label> <!-- $qNumber-->
+        <input type="text" name="question" size="50" maxlength="100" placeholder="Saisissez votre question">
+        <!-- choix type de réponse -->
+        <select name="type" size="1" > <!-- onchange="this.form.submit()" -->
+            <!--<option value="none">type de réponse</option> no value -->
+            <option value="unique">Choix unique</option>
+            <option value="multiple">Choix multiple</option>
+            <option value="text">Zone de text</option>
+        </select>
+        <!-- option de réponse obligatoire (if empty alors invalide) -->
+        <label>obligatoire</label>
+        <input type="checkbox" name="obligatoire" value="TRUE">
+        <br>
+        <input type="submit" name="create_questions" value="Passer aux réponses">
+
+    </form>
+
+    <?php
+    if (isset($_POST['create_questions'])) { //ok
+        if(empty($_POST['obligatoire']))
+        {
+            $obligatoire = 0; // false
+        }
+        else {
+            $obligatoire = 1; // true
+        }	
+        if ($_POST['type'] == "unique") {
+            $_SESSION['type'] = 0; // "unique"
+        }
+        else if ($_POST['type'] == "multiple") {
+            $_SESSION['type'] = 1; // "multiple"
+        }
+        else {
+            $_SESSION['type'] = 2; // "text"
+        }
+
+        $req_questions = $mysqli->query("INSERT INTO questions (`poll`, `question`, `mandatory`,`type`) VALUES ($_SESSION[poll], '$_POST[question]', $obligatoire, $_SESSION[type])");
+
+
+        $req_max = $mysqli->query("SELECT MAX(id) FROM `questions` WHERE `poll` = '$_SESSION[poll]'");
+    	$row = mysqli_fetch_array($req_max);
+        $_SESSION['max'] = $row['MAX(id)']; // Je récupère l'id max
+
+        // récupère l'id de la question
+    	$req_id = $mysqli->query("SELECT id FROM questions WHERE `id` = '$_SESSION[max]'"); // récup le dernier sondage créé
+    	$row = mysqli_fetch_array($req_id); // sous forme de tableau
+    	$_SESSION['choice'] = $row['id']; // Je récupère l'id pour la clé étrangère
+
+        //$req_questions = $mysqli->query("INSERT INTO questions (`poll`, `question`, `mandatory`,`type`) VALUES (33, 'hacked', 1, 2");
+
+        $_SESSION['qNumber']+=1;
+        header('location: answers-creation.php');
     }
-    else {
-        $obligatoire = 1; // true
-    }	
-    if ($_POST['type'] == "unique") {
-        $_SESSION['type'] = 0; // "unique"
-    }
-    else if ($_POST['type'] == "multiple") {
-        $_SESSION['type'] = 1; // "multiple"
-    }
-    else {
-        $_SESSION['type'] = 2; // "text"
-    }
 
-    $req_questions = $mysqli->query("INSERT INTO questions (`poll`, `question`, `mandatory`,`type`) VALUES ($_SESSION[poll], '$_POST[question]', $obligatoire, $_SESSION[type])");
+    ?>
 
+<body>
 
-    $req_max = $mysqli->query("SELECT MAX(id) FROM `questions` WHERE `poll` = '$_SESSION[poll]'");
-	$row = mysqli_fetch_array($req_max);
-    $_SESSION['max'] = $row['MAX(id)']; // Je récupère l'id max
-    
-    // récupère l'id de la question
-	$req_id = $mysqli->query("SELECT id FROM questions WHERE `id` = '$_SESSION[max]'"); // récup le dernier sondage créé
-	$row = mysqli_fetch_array($req_id); // sous forme de tableau
-	$_SESSION['choice'] = $row['id']; // Je récupère l'id pour la clé étrangère
-
-    //$req_questions = $mysqli->query("INSERT INTO questions (`poll`, `question`, `mandatory`,`type`) VALUES (33, 'hacked', 1, 2");
-
-    $_SESSION['qNumber']+=1;
-    header('location: answers-creation.php');
-}
-    
-?>
-
-    <body>
 </html>
