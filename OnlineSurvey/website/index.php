e7801fc1e2193f1d61ffdd39ae2e13ec39d71cc1
AZT <a.zitounitinlot@gmail.com>
2020-06-08 01:25:32 +0200

    redirection apres dl

--- OnlineSurvey/website/index.php
+++ OnlineSurvey/website/index.php
@@ -88,22 +88,26 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
 
+
+
     <div class="container">
       <div class="row">
         <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+        
           <h1 data-aos="fade-up">Créez <span>rapidement</span> et <span>facilement</span> vos sondages</h1>
           <h5 data-aos="fade-up" data-aos-delay="400">Vous avez des questions ? Nous vous donnons des réponses</h5>
           <div data-aos="fade-up" data-aos-delay="800">
             <a href="signin.php" class="btn-get-started scrollto">Inscription gratuite</a>
           </div>
-        </div>
-        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
-          <img src="assets/img/survey2.png" class="img-fluid animated" alt="">
-        </div>
+        </div>  
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">    
+         <img src="assets/img/survey2.png" class="img-fluid animated" alt="">
       </div>
     </div>
+  
 
   </section>
+  
   <!-- End Hero -->
 
   <main id="main">
