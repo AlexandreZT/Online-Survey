38f55e2d8a516b13cff97ff258adf57e5b898ae2
AZT <a.zitounitinlot@gmail.com>
2020-06-08 19:58:40 +0200

    debug multiple analyse

--- OnlineSurvey/website/analyses.php
+++ OnlineSurvey/website/analyses.php
@@ -14,28 +14,23 @@
     $req_analyses = $mysqli->query("SELECT * FROM analyses WHERE `creator`= $_SESSION[id]");
     $req_analyses->data_seek(0);
     while ($row = $req_analyses->fetch_assoc()) { // regarde chaque ligne
-        
-        if (!isset($max_quest))
-        {
-            $req_nb_quest = $mysqli->query("SELECT MAX(question) FROM analyses WHERE `creator`= $_SESSION[id]");
-            $row2 = mysqli_fetch_array($req_nb_quest);
-            $max_quest = $row2['MAX(question)'];
-        }
         array_push($surveyList, $row['survey']); // 51515151515151535353535353
     }
     
     $currentsurv = 0;
     foreach($surveyList as $surv) {
-        
         if($currentsurv != $surv) {
             $currentsurv = $surv;
+            $req_nb_quest = $mysqli->query("SELECT MAX(question) FROM analyses WHERE `creator`= $_SESSION[id] AND `survey`= $currentsurv");
+            $row2 = mysqli_fetch_array($req_nb_quest);
+            $max_quest = $row2['MAX(question)'];
             echo "Sondage n° : $currentsurv </br>";
             $question = 1;
             while ($question <= $max_quest) {
                 $answer = 1;
                 echo "Question $question : ";
                 while ($answer <= 5) {
-                    $req_count = $mysqli->query("SELECT COUNT(answer) FROM analyses WHERE `creator`= $_SESSION[id] AND question = $question AND answer = $answer");
+                    $req_count = $mysqli->query("SELECT COUNT(answer) FROM analyses WHERE `creator`= $_SESSION[id] AND `survey`= $currentsurv AND question = $question AND answer = $answer");
                     // $req_comment = $mysqli->query("SELECT answer FROM analyses WHERE question = $question AND answer NOT LIKE = '%[^0-9]%'");
                     
                     $count_row = $req_count->fetch_assoc();
@@ -45,10 +40,8 @@
                         echo "réponse n°$answer = $test, ";
                      }
                     $answer++;
-                }
-                
-                $req_comment = $mysqli->query("SELECT answer FROM analyses WHERE question = $question");
-                //$comment_row = $req_comment->fetch_assoc();
+                }              
+                $req_comment = $mysqli->query("SELECT answer FROM analyses WHERE `survey`= $currentsurv AND question = $question");
                 while ($comment_row = $req_comment->fetch_assoc()) {
                     if ($comment_row['answer'] != 1 && $comment_row['answer']  != 2 && $comment_row['answer']  != 3 && $comment_row['answer']  != 4 && $comment_row['answer']  != 5) {
                         echo "$comment_row[answer] -";
@@ -59,5 +52,4 @@
             }
         }
     }
-
 ?>
